bfbba1517c88feb946f355662183bdfc5c4d30d8
Adedeji Adebisi <adedejiadebisi01@gmail.com>
2021-07-14 21:20:13 +0000

    Hello to initial set of files
    Signed-off-by: Adedeji Adebisi <adedejiadebisi01@gmail.com>
    Change-Id: I06ec7f78489e13af3911ce566516f24bfd10ed3d

---
+++ subprojects/dbus-top/main.cpp
@@ -0,0 +1,83 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <systemd/sd-event.h>
+
+#include <sdbusplus/bus.hpp>
+
+int WIN_H, WIN_W;
+
+sdbusplus::bus::bus* g_bus;
+
+int Foo(const std::string& dbus_iface, const std::string& desc, int x, int y)
+{
+    // Find all chassis in the system
+    sdbusplus::message::message msg = g_bus->new_method_call(
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths");
+    // Signature of GetSubTreePaths: sias
+    msg.append("/"); // Req Path
+    msg.append(0);   // Depth
+
+    msg.append(std::vector<std::string>{dbus_iface});
+    sdbusplus::message::message reply = g_bus->call(msg, 0);
+    std::vector<std::string> chassisIds;
+
+    char buf[200];
+
+    if (!strcmp(reply.get_signature(), "as"))
+    {
+        reply.read(chassisIds);
+        sprintf(buf, "Found %u %s:", chassisIds.size(), desc.c_str());
+        mvaddstr(y, x, buf);
+        for (int i = 0; i < int(chassisIds.size()); i++)
+        {
+            sprintf(buf, "%d: %s", i, chassisIds[i].c_str());
+            mvaddstr(y + 1 + i, x, buf);
+        }
+    }
+    else
+    {
+        sprintf(buf, "Did not find any chassis, cannot create association");
+        mvaddstr(y, x, buf);
+    }
+    return int(chassisIds.size());
+}
+
+void Test1()
+{
+    initscr();
+    noecho();
+    getmaxyx(stdscr, WIN_H, WIN_W);
+    clear();
+
+    int y = 3, x = 3;
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
+    g_bus = &bus;
+    int ret =
+        Foo("xyz.openbmc_project.Inventory.Item.Chassis", "chassis", x, y);
+    y = y + ret + 2;
+    Foo("xyz.openbmc_project.Inventory.Item.Bmc", "bmc", x, y);
+
+    refresh();
+    getch();
+    endwin();
+    echo();
+}
+
+void PrintHello(){
+    border('|', '|', '-', '-', '+', '+', '+', '+');
+    mvaddstr(1,1,"Hello World");
+    mvaddstr(2,1,"This is the beggining of the proposed dbus-top tool.");
+    mvaddstr(3,1,"Thank you.");
+}
+int main(){
+
+    initscr();			/* Start curses mode 		  */
+    PrintHello();
+    refresh();			/* Print it on to the real screen */
+    getch();			/* Wait for user input */
+    endwin();			/* End curses mode		  */
+
+return 0;
+}
