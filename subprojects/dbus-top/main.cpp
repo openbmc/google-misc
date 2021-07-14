816bf6a423d321dd6be507ee37d718369f0008aa
Adedeji Adebisi <adedejiadebisi01@gmail.com>
2021-07-14 21:20:13 +0000

    Hello to initial set of files
    Signed-off-by: Adedeji Adebisi <adedejiadebisi01@gmail.com>
    Change-Id: I06ec7f78489e13af3911ce566516f24bfd10ed3d

---
+++ subprojects/dbus-top/main.cpp
@@ -0,0 +1,20 @@
+#include <ncurses.h>
+
+void PrintHello()
+{
+    border('|', '|', '-', '-', '+', '+', '+', '+');
+    mvaddstr(1, 1, "Hello World");
+    mvaddstr(2, 1, "This is the beggining of the proposed dbus-top tool.");
+    mvaddstr(3, 1, "Thank you.");
+}
+int main()
+{
+
+    initscr(); /* Start curses mode*/
+    PrintHello();
+    refresh(); /* Print it on to the real screen */
+    getch();   /* Wait for user input */
+    endwin();  /* End curses mode*/
+
+    return 0;
+}
