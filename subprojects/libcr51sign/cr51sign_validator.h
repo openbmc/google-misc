f9b146d8752f868f268f035fdc39d97a53f86d8d
Willy Tu <wltu@google.com>
2021-09-29 17:36:24 -0700

    google-misc: libcr51: add helper library for cr51 validation
    Added
    - readImage()
      - Read the image data into a buffer
    - hashDescriptor()
      - Get the SHA256 of the image descriptor
    - validateDescriptor()
      - Validate the image
    
    Tesetd:
    Validated this library by migrating internal code to link to this and
    is functional.
    
    Change-Id: I78cf250da2f68d6a3be557a9d773170b774ba524
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ subprojects/libcr51sign/cr51sign_validator.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "libcr51sign.h"
+
+#include <optional>
+#include <string_view>
+#include <vector>
+
+namespace google
+{
+namespace cr51
+{
+
+bool readImage(std::string_view fileName, size_t size);
+
+std::optional<std::vector<uint8_t>> hashDescriptor(struct libcr51sign_ctx* ctx);
+
+std::optional<struct libcr51sign_validated_regions>
+    validateDescriptor(struct libcr51sign_ctx* ctx);
+
+} // namespace cr51
+} // namespace google
