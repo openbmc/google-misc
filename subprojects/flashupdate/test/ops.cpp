6fe8841b0f4ab2b764025f2b349d80d85f86f81f
Willy Tu <wltu@google.com>
2021-10-01 11:15:31 -0700

    flashupdate: Implement updateState command
    The updateState command write the flash update state to the EEPROM.
    
    Change-Id: I333219cbaf9e509f5281d57f8b755eb1a828aa5f
    Signed-off-by: Willy Tu <wltu@google.com>

--- subprojects/flashupdate/test/ops.cpp
+++ subprojects/flashupdate/test/ops.cpp
@@ -26,16 +26,29 @@
 namespace flashupdate
 {
 
-TEST(OperationTest, InfoPass)
+class OperationTest : public ::testing::Test
 {
+  protected:
+    OperationTest()
+    {
+        resetInfo();
+    }
+
+    void resetInfo()
+    {
+        updateInfo.active = info::Version(activeVersion);
+        updateInfo.stage = info::Version(stageVersion);
+        updateInfo.stagingIndex = 3;
+        updateInfo.state = 2;
+    }
+
     info::UpdateInfo updateInfo;
     std::string_view activeVersion = "10.11.12.13";
     std::string_view stageVersion = "4.3.2.1";
-    updateInfo.active = info::Version(activeVersion);
-    updateInfo.stage = info::Version(stageVersion);
-    updateInfo.stagingIndex = 3;
-    updateInfo.state = 2;
+};
 
+TEST_F(OperationTest, InfoPass)
+{
     auto ptr = reinterpret_cast<std::byte*>(&updateInfo);
     auto buffer = std::vector<std::byte>(ptr, ptr + sizeof(info::UpdateInfo));
 
@@ -88,6 +101,59 @@ TEST(OperationTest, InfoPass)
                     "CORRUPTED", "0", "00000000000000000000000");
     EXPECT_EQ(printBuffer.str(), expectedOutput);
 
+    std::cout.rdbuf(old);
+}
+
+TEST(OperationTest, UpdateStateInvalidState)
+{
+    Args args;
+
+    args.state = "FAKE_STATE";
+    EXPECT_THROW(
+        try { ops::updateState(args); } catch (const std::runtime_error& e) {
+            EXPECT_STREQ(
+                e.what(),
+                fmt::format(
+                    "{} is not a supported state. Need to be one of\n{}",
+                    args.state)
+                    .c_str());
+            throw;
+        },
+        std::runtime_error);
+}
+
+TEST_F(OperationTest, UpdateStatePass)
+{
+    auto ptr = reinterpret_cast<std::byte*>(&updateInfo);
+    auto buffer = std::vector<std::byte>(ptr, ptr + sizeof(info::UpdateInfo));
+
+    std::string filename = "test.txt";
+    std::ofstream testfile;
+    std::string expectedOutput = "";
+
+    testfile.open(filename, std::ios::out);
+    testfile << buffer.data();
+    testfile.flush();
+    testfile.close();
+
+    Args args;
+    args.config.eeprom.path = filename;
+    args.checkActiveVersion = true;
+    args.checkStageVersion = true;
+    args.checkStageState = true;
+    args.otherInfo = true;
+    args.cleanOutput = true;
+    args.state = "STAGED";
+
+    ops::updateState(args);
+
+    std::stringstream printBuffer;
+    std::streambuf* old = std::cout.rdbuf(printBuffer.rdbuf());
+    ops::info(args);
+    EXPECT_EQ(printBuffer.str(),
+              fmt::format("{}\n{}\n{}\n{}\n{}\n", activeVersion, stageVersion,
+                          "STAGED", "0", "00000000000000000000000"));
+    printBuffer.clear();
     std::cout.rdbuf(old);
 }
 
