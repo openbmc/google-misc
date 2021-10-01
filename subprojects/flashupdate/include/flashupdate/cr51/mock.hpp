3febb6bebbf5e5b80e2bac7a76f2aa54cf4ce190
Willy Tu <wltu@google.com>
2021-10-01 10:55:13 -0700

    flashupdate: Add wrapper for libcr51sign
    Change-Id: Id03c94b43336912714b0063c88df4df587cdebe0
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ subprojects/flashupdate/include/flashupdate/cr51/mock.hpp
@@ -0,0 +1,47 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+#include <libcr51sign/libcr51sign.h>
+
+#include <flashupdate/cr51.hpp>
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <gmock/gmock.h>
+
+namespace flashupdate
+{
+namespace cr51
+{
+
+struct Mock : public Cr51
+{
+    Mock() : Cr51()
+    {}
+
+    MOCK_METHOD(bool, validateImage,
+                (std::string_view file, uint32_t size, Config::Key keys),
+                (override));
+    MOCK_METHOD(std::string, imageVersion, (), (override));
+    MOCK_METHOD(std::vector<struct image_region>, persistentRegions, (),
+                (override));
+    MOCK_METHOD(bool, verify, (bool), (override));
+    MOCK_METHOD(std::vector<uint8_t>, descriptorHash, (), (override));
+    MOCK_METHOD(bool, prodImage, (), (override));
+};
+
+} // namespace cr51
+} // namespace flashupdate
