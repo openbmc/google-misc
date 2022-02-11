4037e705fe092e36effd2c84aa5672660dc41b16
William A. Kennington III <wak@google.com>
2022-02-11 03:08:25 -0800

    ncsid: Use per-interface gateway
    Change-Id: I97185fc181918ad4668cb4e758eec25c51090b04
    Signed-off-by: William A. Kennington III <wak@google.com>

--- subprojects/ncsid/src/update_ra_gw.sh
+++ subprojects/ncsid/src/update_ra_gw.sh
@@ -28,7 +28,7 @@ set_rtr() {
   (( lifetime > 0 )) || return
 
   echo "Setting default router: $rtr at $mac" >&2
-  UpdateGateway xyz.openbmc_project.Network "$rtr" || return
+  UpdateGateway xyz.openbmc_project.Network "$NCSI_IF" "$rtr" || return
   UpdateNeighbor xyz.openbmc_project.Network "$NCSI_IF" "$rtr" "$mac" || return
 
   retries=-1
@@ -46,8 +46,6 @@ while true; do
   else
     args+=(-d)
   fi
-  echo "Starting" >&2
-  set -x
   while read line; do
     if [ -z "$line" ]; then
       lifetime=
