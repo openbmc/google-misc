011938e111c1fc4269cb19c2e8bbf74887776242
Michael Shen <gpgpgp@google.com>
2022-06-09 05:29:38 +0000

    btmanager: Add a service to record the BMC poweroff timestamp
    Signed-off-by: Michael Shen <gpgpgp@google.com>
    Change-Id: Ibf162498237f90816367a01c3149b41d033176b6

---
+++ subprojects/btmanager/service_files/record-down-end.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# jq
+# https://stackoverflow.com/questions/42011086/merge-arrays-of-json
+# jq -s '.[0].list=[.[].list|add]|.[0]' *.json
+
+
+# systemd
+# https://unix.stackexchange.com/questions/39226/how-to-run-a-script-with-systemd-right-before-shutdown
