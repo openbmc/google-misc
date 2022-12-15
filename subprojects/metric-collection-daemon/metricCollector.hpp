35423adf63672c8a6b43c03b50c4ce2fd2f15365
Edward Lee <edwarddl@google.com>
2022-12-15 20:01:57 +0000

    Metric Collection Daemon
    Testing:
    I grab the binary after compiling and copy it on the bmc.
    Then I just run the binary.
    
    In a separate terminal I run these commands
    ```
    busctl introspect xyz.openbmc_project.Metric /xyz/openbmc_project/metric/bmc0   xyz.openbmc_project.Metric.BMC
    busctl get-property xyz.openbmc_project.Metric  /xyz/openbmc_project/metric/bmc0  xyz.openbmc_project.Metric.BMC  BootCount
    
    busctl introspect xyz.openbmc_project.Metric /xyz/openbmc_project/metric/bmc0/eth0
    busctl introspect xyz.openbmc_project.Metric /xyz/openbmc_project/metric/bmc0/5910
    
    busctl get-property xyz.openbmc_project.ObjectMapper /xyz/openbmc_project/metric/bmc0/port xyz.openbmc_project.Association endpoints
    busctl get-property xyz.openbmc_project.ObjectMapper /xyz/openbmc_project/metric/bmc0/daemon xyz.openbmc_project.Association endpoints
    ```
    
    Signed-off-by: Edward Lee <edwarddl@google.com>
    Change-Id: I7065d4f3642a46f3a730a755485bf9162d94710f

---
+++ subprojects/metric-collection-daemon/metricCollector.hpp
@@ -0,0 +1,78 @@
+#include "daemon.hpp"
+#include "port.hpp"
+#include "utils.hpp"
+
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <linux/if_link.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <syslog.h>
+#include <unistd.h>
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/message.hpp>
+#include <sdbusplus/server/manager.hpp>
+#include <sdbusplus/unpack_properties.hpp>
+#include <sdeventplus/clock.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
+
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <tuple>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+const std::string serviceName = "xyz.openbmc_project.Metric";
+const std::string bmcObjectPath = "/xyz/openbmc_project/metric/bmc0";
+const std::string bmcInterfaceName = "xyz.openbmc_project.Metric.BMC";
+const std::string associationInterfaceName =
+    "xyz.openbmc_project.Association.Definitions";
+
+constexpr std::string_view procPath = "/proc/";
+
+class MetricCollector
+{
+  public:
+    MetricCollector(boost::asio::io_context& ioc,
+                    std::shared_ptr<sdbusplus::asio::connection> conn,
+                    sdbusplus::asio::object_server& objServer);
+
+    void registerPorts();
+    void registerAssociations();
+    void addPortAssociations(std::vector<Association>& associations);
+    void addDaemonAssociations(std::vector<Association>& associations);
+    void removeAssociation(const std::string objectPath);
+
+    std::optional<std::string> getDaemonObjectPathFromPid(const int pid);
+    void registerDaemons();
+
+    void updatePorts();
+    void updateDaemons();
+    void update();
+
+    void updateBootCount();
+    int getNumPorts();
+
+  private:
+    boost::asio::io_context& ioc;
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+    sdbusplus::asio::object_server& objServer;
+
+    size_t bootCount;
+    size_t crashCount;
+    std::unordered_map<std::string, Port> bmcPorts;
+    std::unordered_map<int, Daemon> bmcDaemons;
+    std::vector<Association> associations;
+};
