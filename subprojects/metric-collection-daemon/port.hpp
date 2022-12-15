7851ee86223733bf8affee00b992dbf81a8187d0
Edward Lee <edwarddl@google.com>
2022-12-15 20:01:57 +0000

    Metric Collection Daemon
    Testing:
    I grab the binary after compiling and copy it on the bmc.
    Then I just run the binary.
    
    In a separate terminal I run these commands
    ```
    busctl introspect xyz.openbmc_project.Metric /xyz/openbmc_project/metric/bmc0   xyz.openbmc_project.Metric.BMC
    busctl get-property xyz.openbmc_project.Metric  /xyz/openbmc_project/metric/bmc0  xyz.openbmc_project.Metric.BMC  BootCount
    
    busctl introspect xyz.openbmc_project.Metric /xyz/openbmc_project/metric/bmc0/eth0
    busctl introspect xyz.openbmc_project.Metric /xyz/openbmc_project/metric/bmc0/5910
    
    busctl get-property xyz.openbmc_project.ObjectMapper /xyz/openbmc_project/metric/bmc0/port xyz.openbmc_project.Association endpoints
    busctl get-property xyz.openbmc_project.ObjectMapper /xyz/openbmc_project/metric/bmc0/daemon xyz.openbmc_project.Association endpoints
    ```
    
    Signed-off-by: Edward Lee <edwarddl@google.com>
    Change-Id: I7065d4f3642a46f3a730a755485bf9162d94710f

---
+++ subprojects/metric-collection-daemon/port.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "utils.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/message.hpp>
+
+const std::string portInterfaceName = "xyz.openbmc_project.Metric.Port";
+
+class Port
+{
+
+    enum class LinkState : uint8_t
+    {
+        LinkDown_Disabled = 0,
+        LinkDown_Enabled,
+        LinkUp_Disabled,
+        LinkUp_Enabled,
+    };
+
+    std::string linkStateToString(LinkState state);
+
+  public:
+    Port(boost::asio::io_context& ioc,
+         std::shared_ptr<sdbusplus::asio::connection> conn,
+         sdbusplus::asio::object_server& objServer, const std::string bmcPath,
+         const std::string portId);
+    ~Port();
+
+    size_t getSpeed(std::string interfaceName);
+    bool getEnabled(std::string interfaceName);
+    bool getLinkUp(std::string interfaceName);
+    LinkState getLinkState(std::string interfaceName);
+    void updatePortInfo();
+    void updatePortProperties();
+
+    const std::string getObjectPath() const;
+    const std::string getPortId() const;
+    size_t getTxDroppedPackets() const;
+    size_t getRxDroppedPackets() const;
+
+  private:
+    boost::asio::io_context& ioc;
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+    sdbusplus::asio::object_server& objServer;
+    std::string objectPath;
+    const std::string portId;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> metrics;
+
+    LinkState linkState;
+    size_t txDroppedPackets;
+    size_t rxDroppedPackets;
+    size_t speed;
+};
