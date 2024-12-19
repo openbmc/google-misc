eba09a474a6ba2fecbcbcd1ab1209bbfdda45b89
Mo Elbadry <elbadrym@google.com>
2024-12-19 18:40:21 +0000

    dhcp-done: Support dhcp Structured Data
    This code is to support sending structured data during
    installation.
    
    Change-Id: I41a2e89b5beaf3d08f89d6c1eb277bd74591bf36
    Signed-off-by: Mo Elbadry <elbadrym@google.com>

--- subprojects/dhcp-done/update-dhcp-status.cpp
+++ subprojects/dhcp-done/update-dhcp-status.cpp
@@ -10,6 +10,12 @@ static void printUsage()
     stdplus::println(stderr, "Usage: update_dhcp_status <state> <message>");
     stdplus::println(stderr,
                      "<state> is one of 'DONE', 'POWERCYCLE' or 'ONGOING'");
+    stdplus::println(
+        stderr,
+        "New Usage: update_dhcp_status <State> <Time> <Message> <Error Code> <Next State> <Retries>");
+    stdplus::println(
+        stderr,
+        "Note: <Retries> is optional, hardcoded to 2 logging unlike legacy state");
 }
 
 static int genStatusCode(char* state)
@@ -30,14 +36,8 @@ static int genStatusCode(char* state)
     return -1;
 }
 
-int main(int argc, char* argv[])
+static int ThreeArgsProc(char* argv[])
 {
-    if (argc != 3)
-    {
-        printUsage();
-        return 1;
-    }
-
     int statusCode = genStatusCode(argv[1]);
 
     if (statusCode == -1)
@@ -58,6 +58,55 @@ int main(int argc, char* argv[])
         stdplus::println(stderr, "Failed to update status file {}", e.what());
         return 1;
     }
-
     return 0;
+}
+
+int StructuredArgsProc(int argc, char* argv[])
+{
+    try
+    {
+        std::string status;
+        status.push_back(2);
+        status.append("{'State':'");
+        status.append(argv[1]);
+        status.append("','Time':");
+        status.append(argv[2]);
+        status.append("','Message':");
+        status.append(argv[3]);
+        status.append("','Error Code':");
+        status.append(argv[4]);
+        status.append("','Next State':");
+        status.append(argv[5]);
+        if (argc > 6)
+        {
+            status.append("','retries':");
+            status.append(argv[6]);
+        }
+        status.append("'}");
+        fileWrite(statusFile, status);
+    }
+    catch (const std::exception& e)
+    {
+        stdplus::println(stderr, "Failed to update status file {}", e.what());
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3 && argc < 6)
+    {
+        printUsage();
+        return 1;
+    }
+
+    if (argc == 3)
+    {
+        return ThreeArgsProc(argv);
+    }
+    else
+    {
+        return StructuredArgsProc(argc, argv);
+    }
 }
