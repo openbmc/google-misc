c9843fb6fbae33e65bdbaf097222d8e25185ec5f
Mo Elbadry <elbadrym@google.com>
2024-12-19 18:40:21 +0000

    dhcp-done: add send-dhcp-status to support structured data
    Change-Id: I41a2e89b5beaf3d08f89d6c1eb277bd74591bf36
    Signed-off-by: Mo Elbadry <elbadrym@google.com>

---
+++ subprojects/dhcp-done/send-dhcp-status.cpp
@@ -0,0 +1,49 @@
+#include "file-io.hpp"
+
+#include <stdplus/print.hpp>
+
+#include <cstring>
+#include <iostream>
+
+static void printUsage()
+{
+    stdplus::println(stderr, "Usage: send_dhcp_status <State> <Time> <Message> <Error Code> <Next State> <Retries>");
+    stdplus::println(stderr, "Note: <Retries> is optional");
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 6)
+    {
+        printUsage();
+        return 1;
+    }
+
+    try
+    {
+        std::string status;
+        status.append("{'State':");
+        status.append(argv[1]);
+        status.append(",'Time':");
+        status.append(argv[2]);
+        status.append(",'Message:'");
+	status.append(argv[3]);
+        status.append(",'Error Code':");
+        status.append(argv[4]);
+        status.append(",'Next State':");
+        status.append(argv[5]);
+        if (argc > 6){
+          status.append(",'retries':");
+          status.append(argv[6]);
+	}
+        status.append("}");
+        fileWrite(statusFile, status);
+    }
+    catch (const std::exception& e)
+    {
+        stdplus::println(stderr, "Failed to update status file {}", e.what());
+        return 1;
+    }
+
+    return 0;
+}
